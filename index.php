20eaed25853a2de7a22a0e92b2d79f686420bfda
Adam Culp <adamculp@uws.net>
2020-06-02 12:51:21 -0400

    Added initial application/function.
    Signed-off-by: Adam Culp <adamculp@uws.net>

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Aws\Sqs\SqsClient;
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+use Aws\Exception\AwsException;
+
+return function ($event) {
+
+    $message = $event['Records'][0];
+    $queueUrl = "<your-sqs-url>";
+    $DynamoDbTableName = '<your-dynamodb-table-name>';
+    $region = 'us-east-1';
+    $version = 'latest';
+
+    $sqsClient = new SqsClient([
+        'region' => $region,
+        'version' => $version
+    ]);
+
+    $dynamoClient = new DynamoDbClient([
+        'region' => $region,
+        'version' => $version
+    ]);
+
+    // connect to DynamoDB, add the record, and remove from SQS
+    if (!empty($message)) {
+        try {
+            $marshaller = new Marshaler();
+            $item = $marshaller->marshalJson(json_encode($message));
+
+            $params = [
+                'TableName' => $DynamoDbTableName,
+                'Item' => $item
+            ];
+
+            $response = $dynamoClient->putItem($params);
+            echo "Added item!\n";
+
+        } catch (DynamoDbException $e) {
+            echo "Unable to add item:\n";
+            echo $e->getMessage() . "\n";
+        }
+
+        // remove record from SQS as its confirmed in dynamo
+        try {
+            if ($response->statusCode = 200) {
+                $sqsClient->deleteMessage([
+                    'QueueUrl' => $queueUrl, // REQUIRED
+                    'ReceiptHandle' => $message['receiptHandle']
+                ]);
+            }
+
+        } catch (AwsException $e) {
+            error_log($e->getMessage());
+        }
+
+    } else {
+        echo "No message in queue. \n";
+    }
+
+    return "Message moved to DynamoDB: " . $message['messageId'];
+};
