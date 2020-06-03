89a3ee46a2d27ec5d16909ea5f5dde68a94285a7
Adam Culp <adamculp@uws.net>
2020-06-02 20:49:35 -0400

    Updated to iterate over possible multiple submissions in same event, and removed the deletion of message because it automatically happens.
    Signed-off-by: Adam Culp <adamculp@uws.net>

--- index.php
+++ index.php
@@ -13,17 +13,9 @@ use Aws\Exception\AwsException;
 return function ($event) {
 
     // UPDATE THESE VARIABLES AS NEEDED
-    $queueUrl = "<your-sqs-url>";
     $DynamoDbTableName = '<your-dynamodb-table-name>';
     $region = 'us-east-1';
     $version = 'latest';
-
-    $message = $event['Records'][0];
-
-    $sqsClient = new SqsClient([
-        'region' => $region,
-        'version' => $version
-    ]);
 
     $dynamoClient = new DynamoDbClient([
         'region' => $region,
@@ -31,40 +23,29 @@ return function ($event) {
     ]);
 
     // connect to DynamoDB, add the record, and remove from SQS
-    if (!empty($message)) {
-        try {
-            $marshaller = new Marshaler();
-            $item = $marshaller->marshalJson(json_encode($message));
+    if (!empty($event['Records'])) {
+        foreach ($event['Records'] as $value) {
+            try {
+                $marshaller = new Marshaler();
+                $item = $marshaller->marshalJson(json_encode($value));
 
-            $params = [
-                'TableName' => $DynamoDbTableName,
-                'Item' => $item
-            ];
+                $params = [
+                    'TableName' => $DynamoDbTableName,
+                    'Item' => $item
+                ];
 
-            $response = $dynamoClient->putItem($params);
-            echo "Added item!\n";
+                $response = $dynamoClient->putItem($params);
+                echo "Added item!\n";
 
-        } catch (DynamoDbException $e) {
-            echo "Unable to add item:\n";
-            echo $e->getMessage() . "\n";
-        }
-
-        // remove record from SQS as its confirmed in dynamo
-        try {
-            if ($response->statusCode = 200) {
-                $sqsClient->deleteMessage([
-                    'QueueUrl' => $queueUrl, // REQUIRED
-                    'ReceiptHandle' => $message['receiptHandle']
-                ]);
+            } catch (DynamoDbException $e) {
+                echo "Unable to add item:\n";
+                echo $e->getMessage() . "\n";
             }
-
-        } catch (AwsException $e) {
-            error_log($e->getMessage());
         }
 
     } else {
         echo "No message in queue. \n";
     }
 
-    return "Message moved to DynamoDB: " . $message['messageId'];
+    return "Message moved to DynamoDB!";
 };
